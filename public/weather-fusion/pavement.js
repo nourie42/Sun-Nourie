3ca51b4160d8fa2bef3f8c95f4672f948b53f0e6
nourie42 <nourie42@gmail.com>
2026-09-18 18:11:34 -0400

    Refresh live-rain pet art

--- public/weather-fusion/pavement.js
+++ public/weather-fusion/pavement.js
@@ -1,5 +1,5 @@
 import {finite,solarElevation,thermalHumidity} from './weather-math.js?v=weather-qa-v67';
-import {clothingForFeels,referenceScene,precipitationActivity} from './exposure-scene.js?v=weather-qa-v67';
+import {clothingForFeels,referenceScene,precipitationActivity} from './exposure-scene.js?v=rain-now-v71';
 import {thermalRisk} from './thermal-risk.js?v=weather-art-labels-v10';
 import {weatherShapes} from './weather-display.js?v=weather-qa-v67';
 import {weatherState} from './weather-state.js';
